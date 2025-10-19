701e1203f31422e939fdd01151abc436f17ae24b
Maroof Ali Syed <maroof@Maroofs-MacBook-Air-M4.local>
2025-10-19 13:59:06 +0530

    Added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,6 +51,19 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-N7KLL52NVH"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-N7KLL52NVH');
+    `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
